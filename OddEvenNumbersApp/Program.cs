88e9ea1c0e36d3d36908e1f9a38e98170464a382
mosuab ibra <mosuab.meso123@gmail.com>
2024-10-02 17:26:15 +0300

    initial commit

---
+++ OddEvenNumbersApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OddEvenNumbersApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            PrintNumbers("Numbers : ", numbers);
+            PrintNumbers("Even Numbers  ", numbers.Where(n => IsEven(n)));
+            PrintNumbers("Odd Numbers  ", numbers.Where(n => IsOdd(n)));
+
+
+            Console.ReadLine();
+        }
+
+        static void PrintNumbers(string title,IEnumerable<int> numbers)
+        {
+            Console.WriteLine();
+            Console.Write($"{title}: [");
+            foreach (int n in numbers)
+            {
+                Console.Write($" {n}");
+            }
+
+            Console.Write(" ]");
+            Console.WriteLine();
+        }
+
+        static bool IsEven(int number)
+        {
+            if(number%2==0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool IsOdd(int number)
+        {
+            if (number % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
